817c651dac22b2e51a00897e1d9bc4506f8b273a
mqkhan125 <muhammadqasimkhan341@gmail.com>
2026-06-02 20:03:37 +0500

    feat: add API routes for student and admin

---
+++ backend/routes/route.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const router = express.Router()
+
+const auth = require('../middleware/auth');
+const validation = require('../middleware/validation')
+const {isStudent, isAdmin} = require('../middleware/role')
+
+// student route
+router.get('/students', auth, validation, isStudent, (req, res) => {
+    res.json({
+      success: true,
+      message: "Student Access Granted",
+      role: "student",
+    });
+})
+
+// admin route 
+router.get('/admin', auth, validation, isAdmin, (req, res) => {
+    res.json({
+        success : true,
+        message : "Admin Access Granted",
+        role : 'admin'
+    })
+})
+
+module.exports = router
+
