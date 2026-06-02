f95f731d95fa5f746b297d1f95a4b3e78d600a83
mqkhan125 <muhammadqasimkhan341@gmail.com>
2026-06-03 00:19:55 +0500

    feat: build middleware demo dashboard UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,50 @@
+import { useState } from "react"
+import axios from 'axios'
 
 
-function App() {
+
+const App = () => {
+
+  const [data, setData] = useState(null)
+
+  const callApi = async(type) => {
+    const res = await axios(`http://localhost:5000/api/${type}`);
+    setData(res)
+  }  
 
   return (
-   <div className="bg-red-600">app</div>
-  )
+    <div className="h-screen flex flex-col items-center justify-center bg-gray-100">
+      <h1 className="text-3xl font-bold mb-6">Middleware Demo</h1>
+      <div className="flex gap-4">
+        <button
+          onClick={() => callApi("student")}
+          className="bg-blue-500 text-white px-4 py-2 rounded"
+        >
+          Student
+        </button>
+
+        <button
+          onClick={() => callApi("admin")}
+          className="bg-red-500 text-white px-4 py-2 rounded"
+        >
+          Admin
+        </button>
+      </div>
+      {data && (
+        <div className="mt-6 p-4 bg-white shadow rounded w-80">
+          <p>
+            <b>Message:</b> {data.message}
+          </p>
+          <p>
+            <b>Role:</b> {data.role}
+          </p>
+          <p>
+            <b>Status:</b> {data.success ? "Success" : "Failed"}
+          </p>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App
